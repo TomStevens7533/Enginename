52a4d843b5e0e2547ce30bf33ff57fd84d595e6f
TomStevens7533 <34093176+TomStevens7533@users.noreply.github.com>
2022-02-18 12:54:41 +0100

    F - DeltaTime support added

---
+++ Minigin/Time.cpp
@@ -0,0 +1,17 @@
+#include "Time.h"
+
+namespace dae {
+	Time::Time()
+	{
+		m_StartTime = std::chrono::high_resolution_clock::now();
+	}
+	float Time::GetDeltaTime()
+	{
+		return m_DeltaTime;
+	}
+	void Time::Update()
+	{
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		m_DeltaTime = std::chrono::duration<float>(std::chrono::high_resolution_clock::now() - m_StartTime).count();
+	}
+}
--- Minigin/Time.h
+++ Minigin/Time.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Singleton.h"
+#include <chrono>
+
+namespace dae {
+	class Time : public Singleton<Time>
+	{
+	public:
+		Time();
+		~Time() = default;
+
+		float GetDeltaTime();
+		void Update();
+	private:
+		std::chrono::steady_clock::time_point  m_StartTime;
+		float m_DeltaTime;
+	};
+}
